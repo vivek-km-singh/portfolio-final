8e45ca148df4a92cee81ff1c643c4a75df6d2df5
vivek singh <viveksingh9000rk@gmail.com>
2022-12-04 12:31:31 +0530

    uploading new

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose");
 
 // creating a database 
-mongoose.connect("mongodb://localhost:27017/newportfoliochat",{
+mongoose.connect("mongodb+srv://viveksingh900vs:Mm12345@#@cluster0.ioturlf.mongodb.net/?retryWrites=true&w=majority",{
     useCreateIndex : true,
     useNewUrlParser:true,
     useUnifiedTopology:true
